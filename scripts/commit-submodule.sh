92d418123bb00db8a1fad3b36878f4a1a653a361
lorencerri <lorencerri@gmail.com>
2022-06-02 15:46:08 -0700

    Update commit-submodule.sh

--- scripts/commit-submodule.sh
+++ scripts/commit-submodule.sh
@@ -2,7 +2,8 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "Usage: commit-submodule.sh <submodule-name> <commit-message>"
 else
 	cd "$1"
-	git commit -a -m "$2"
+	git add .
+	git commit -m "$2"
 	git push
 	cd ..
 	git add "$1"
