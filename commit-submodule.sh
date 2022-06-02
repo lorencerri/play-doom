630ca44cc272cb1fd7d24aa1ef7c724d0b8c986a
Loren Cerri <lorencerri@gmail.com>
2022-06-02 17:19:08 -0400

    Update commit-submodule.sh

--- commit-submodule.sh
+++ commit-submodule.sh
@@ -2,24 +2,10 @@
 
 if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "Usage: commit-submodule.sh <submodule-name> <commit-message>"
-	close
+else
+	cd "$1" && git commit -a -m "$2" && git push && cd .. && git add "$1" && git commit -m "Updated submodule $1"
 fi
 
-if [ ! -d "$1" ]; then
-	echo "Submodule $1 not found"
-	close
-fi
-
-cd "$1"
-git commit -a -m "$2"
-git push
-cd ..
-git add "$1"
-git commit -m "Updated submodule $1"
-close
-
-close () {
-	echo "press any key to exit"
-	read -n 1
-	exit 1
-}
+echo "press any key to exit"
+read -n 1
+exit 1
