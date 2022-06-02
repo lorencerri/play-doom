f3b26b27c91ef8f607a9ed1d1bbeac86c95bb856
Loren Cerri <lorencerri@gmail.com>
2022-06-02 17:34:30 -0400

    Update .gitignore

--- commit-submodule.sh
+++ commit-submodule.sh
@@ -3,7 +3,12 @@
 if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "Usage: commit-submodule.sh <submodule-name> <commit-message>"
 else
-	cd "$1" && git commit -a -m "$2" && git push && cd .. && git add "$1" && git commit -m "Updated submodule $1"
+	cd "$1"
+	git commit -a -m "$2"
+	git push
+	cd ..
+	git add "$1"
+	git commit -m "Updated submodule $1"
 fi
 
 echo "press any key to exit"
