29721d8d57870d4ca8f4c511471a0a5ce7ffdbe0
Loren Cerri <lorencerri@gmail.com>
2022-06-02 17:16:21 -0400

    Remove screenshots

--- commit-submodule.sh
+++ commit-submodule.sh
@@ -2,17 +2,24 @@
 
 if [ -z "$1" ] || [ -z "$2" ]; then
 	echo "Usage: commit-submodule.sh <submodule-name> <commit-message>"
-	echo "press any key to exit"
-	read -n 1
-	exit 1
+	close
 fi
 
-cd doomreplay
+if [ ! -d "$1" ]; then
+	echo "Submodule $1 not found"
+	close
+fi
+
+cd "$1"
 git commit -a -m "$2"
 git push
 cd ..
 git add "$1"
 git commit -m "Updated submodule $1"
+close
 
-echo "press any key to exit"
-read -n 1
+close () {
+	echo "press any key to exit"
+	read -n 1
+	exit 1
+}
